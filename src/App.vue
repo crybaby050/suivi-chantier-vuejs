f52dc7c3167d5842c292e31c4055f5167d12d178
crybaby05 <benthiam750@gmail.com>
2026-08-19 12:03:52 +0000

    Ajout de la logique de restauration de session au démarrage avec la méthode fetchMe du store d'authentification

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+
+// Restaure la session au démarrage (token présent dans localStorage)
+onMounted(() => {
+  auth.fetchMe()
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <RouterView />
 </template>
-
-<style scoped></style>
