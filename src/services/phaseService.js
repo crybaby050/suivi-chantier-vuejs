afa1d305e5ce5e16d9df5237daf0dc76babe52ad
crybaby05 <benthiam750@gmail.com>
2026-08-23 12:12:09 +0000

    Mise à jour des méthodes de modification dans phaseService et tacheService pour utiliser PATCH au lieu de PUT.

--- src/services/phaseService.js
+++ src/services/phaseService.js
@@ -4,7 +4,7 @@ const phaseService = {
     async listerParProjet(projetId) { return (await api.get(`/projets/${projetId}/phases`)).data },
     async detail(id) { return (await api.get(`/phases/${id}`)).data },
     async creer(projetId, data) { return (await api.post(`/projets/${projetId}/phases`, data)).data },
-    async modifier(id, data) { return (await api.put(`/phases/${id}`, data)).data },
+    async modifier(id, data) { return (await api.patch(`/phases/${id}`, data)).data },
     async supprimer(id) { return (await api.delete(`/phases/${id}`)).data },
 }
 
--- src/services/tacheService.js
+++ src/services/tacheService.js
@@ -4,7 +4,7 @@ const tacheService = {
     async listerParPhase(phaseId) { return (await api.get(`/phases/${phaseId}/taches`)).data },
     async detail(id) { return (await api.get(`/taches/${id}`)).data },
     async creer(phaseId, data) { return (await api.post(`/phases/${phaseId}/taches`, data)).data },
-    async modifier(id, data) { return (await api.put(`/taches/${id}`, data)).data },
+    async modifier(id, data) { return (await api.patch(`/taches/${id}`, data)).data },
     async supprimer(id) { return (await api.delete(`/taches/${id}`)).data },
 }
 
